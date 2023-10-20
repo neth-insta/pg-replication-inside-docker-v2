e897704801141d2071ca5fe3745521ce51324a55
DESKTOP-DVSOG5R\Neth Insta <khon.channeth@gmail.com>
2023-10-20 15:15:04 +0700

    Update

--- master/setup-master.sh
+++ master/setup-master.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
@@ -11,4 +10,5 @@ archive_command = 'cd .'
 max_wal_senders = 8
 wal_keep_segments = 8
 hot_standby = on
+primary_conninfo = 'host=10.0.20.151 port=5432 user=$PG_REP_USER password=$PG_REP_PASSWORD'
 EOF
