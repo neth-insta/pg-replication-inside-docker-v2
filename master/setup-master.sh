1041fdfd3a5778906d1d98b649f3f2638c8adbd7
DESKTOP-DVSOG5R\Neth Insta <khon.channeth@gmail.com>
2023-10-22 11:06:13 +0700

    Test

--- master/setup-master.sh
+++ master/setup-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "host replication all 10.0.20.151/24 md5" >> "$PGDATA/pg_hba.conf"
+echo "host replication all $MASTER_DB_HOST/24 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
 CREATE USER $PG_REP_USER REPLICATION LOGIN CONNECTION LIMIT 100 ENCRYPTED PASSWORD '$PG_REP_PASSWORD';
--- slave/docker-entrypoint.sh
+++ slave/docker-entrypoint.sh
@@ -2,12 +2,12 @@
 if [ ! -s "$PGDATA/PG_VERSION" ]; then
 echo "*:*:*:$PG_REP_USER:$PG_REP_PASSWORD" > ~/.pgpass
 chmod 0600 ~/.pgpass
-until ping -c 1 -W 1 10.0.20.151
+until ping -c 1 -W 1 ${MASTER_DB_HOST}
 do
 echo "Waiting for master to ping..."
 sleep 1s
 done
-until pg_basebackup -h 10.0.20.151 -D ${PGDATA} -U ${PG_REP_USER} -vP -W
+until pg_basebackup -h ${MASTER_DB_HOST} -D ${PGDATA} -U ${PG_REP_USER} -vP -W
 do
 echo "Waiting for master to connect..."
 sleep 1s
@@ -16,7 +16,7 @@ echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
 set -e
 cat > ${PGDATA}/recovery.conf <<EOF
 standby_mode = on
-primary_conninfo = 'host=10.0.20.151 port=5432 user=$PG_REP_USER password=$PG_REP_PASSWORD'
+primary_conninfo = 'host=$MASTER_DB_HOST port=$MASTER_DB_PORT user=$PG_REP_USER password=$PG_REP_PASSWORD'
 trigger_file = '/tmp/touch_me_to_promote_to_me_master'
 EOF
 chown postgres. ${PGDATA} -R
